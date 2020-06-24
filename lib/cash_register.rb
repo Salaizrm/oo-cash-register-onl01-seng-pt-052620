9b3ba9a585c7060d2ab44d13f0d4957dbd25b358
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-24 00:46:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,9 +2,9 @@ class CashRegister
 
   attr_accessor :total, :discount, :items
 
-  def initialize(total = 0)
-    @total = total
-    @discount = discount(20)
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
     @items = []
   end
 
