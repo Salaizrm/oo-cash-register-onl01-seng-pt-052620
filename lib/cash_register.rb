18a3cfa927ab0391d5fe00644b87376d4733aad7
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-24 00:53:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :last_transaction
 
   def initialize(discount = 0)
     @total = 0
@@ -13,6 +13,7 @@ class CashRegister
     quantity.times do
       items << product
     end
+    self.last_transaction = price * quantity
   end
 
   def apply_discount
@@ -25,7 +26,7 @@ class CashRegister
   end
 
   def void_last_transaction
-
+    self.total = self.total - self
   end
 
 end
